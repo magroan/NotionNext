8ff2385b9ffb92e700aa829f8bbee8431d896b11
magroan <56810316+magroan@users.noreply.github.com>
2025-11-20 11:06:05 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'asami', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '情報科の教員', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://asami.chiba.jp', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '情報,高校,情報科,ICT,教育,情報教育', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '情報,高校,情報科,ICT,情報学,DX,教育,情報教育', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || '', // 备案查询链接，如果用了萌备等备案请在这里填写
