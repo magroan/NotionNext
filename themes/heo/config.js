51b316eede3f62f028755cb3d4856553e8c71a6f
magroan <56810316+magroan@users.noreply.github.com>
2025-11-23 03:03:43 +0900

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-  { title: 'welcome.', url: 'https://sub.asami.chiba.jp' },
-  { title: 'hogehoge', url: 'https://sub.asami.chiba.jp' }],
+  { title: 'welcome.', url: 'https://asami.chiba.jp' },
+  { title: 'hogehoge', url: 'https://asami.chiba.jp' }],
 
 
   // 英雄区左右侧组件颠倒位置
