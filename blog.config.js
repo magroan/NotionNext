7d22842ff29c2b3145c653d522e161bef1d53e61
magroan <56810316+magroan@users.noreply.github.com>
2025-11-22 21:22:54 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://tanghh.notion.site/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID || '2b0b219f5e518183945deb507bf944fa',
-  THEME: process.env.NEXT_PUBLIC_THEME || 'heo', // 当前主?，在themes文件?下可找到所有支持的主?；主?名称就是文件?名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
+  THEME: process.env.NEXT_PUBLIC_THEME || 'example', // 当前主?，在themes文件?下可找到所有支持的主?；主?名称就是文件?名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'ja-JP', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
 
