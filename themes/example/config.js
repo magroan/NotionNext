51d6f578892639bf50eec8fd009c335a04c9f3fb
magroan <56810316+magroan@users.noreply.github.com>
2025-11-22 21:59:05 +0900

    Update config.js

--- themes/example/config.js
+++ themes/example/config.js
@@ -1,19 +1,19 @@
 /**
- * 主题配置文件
+ * テーマ設定ファイル
  */
 const CONFIG = {
-  // 菜单配置
-  EXAMPLE_MENU_CATEGORY: true, // 显示分类
-  EXAMPLE_MENU_TAG: true, // 显示标签
-  EXAMPLE_MENU_ARCHIVE: true, // 显示归档
-  EXAMPLE_MENU_SEARCH: true, // 显示搜索
+  // メニュー設定
+  EXAMPLE_MENU_CATEGORY: true, // カテゴリを表示
+  EXAMPLE_MENU_TAG: true, // タグを表示
+  EXAMPLE_MENU_ARCHIVE: true, // アーカイブを表示
+  EXAMPLE_MENU_SEARCH: true, // 検索を表示
 
-  EXAMPLE_POST_LIST_COVER: true, // 列表显示文章封面
+  EXAMPLE_POST_LIST_COVER: true, // 一覧に記事のカバー画像を表示
 
-  EXAMPLE_TITLE_IMAGE: false, // 标题栏，是否背景图片
+  EXAMPLE_TITLE_IMAGE: false, // タイトルバーに背景画像を使用するか
 
-  // 文章页面布局
-  EXAMPLE_ARTICLE_LAYOUT_VERTICAL: false, // 文章详情，左右布局改为上下布局
-  EXAMPLE_ARTICLE_HIDDEN_NOTIFICATION: false // 文章详情隐藏公告
+  // 記事ページのレイアウト
+  EXAMPLE_ARTICLE_LAYOUT_VERTICAL: false, // 記事詳細を左右レイアウトではなく上下レイアウトに変更
+  EXAMPLE_ARTICLE_HIDDEN_NOTIFICATION: false // 記事詳細でお知らせを非表示にする
 }
 export default CONFIG
