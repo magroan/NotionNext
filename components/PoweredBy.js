233f4c930dacf873a503f76e66dcb01ec35c054d
asami-hoge <asami@trpger.us>
2025-11-24 13:57:41 +0900

    asami mit

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -7,14 +7,25 @@ import { siteConfig } from '@/lib/config'
 export default function PoweredBy(props) {
   return (
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
-      <span className='mr-1'>Powered by</span>
+      <span className='mr-1'>Based on</span>
       <a
-        href='https://github.com/magroan/NotionNext'
+        href='https://github.com/tangly1024/NotionNext'
         className='underline justify-start'
+        target='_blank'
+        rel='noreferrer'
       >
         NotionNext {siteConfig('VERSION')}
       </a>
-      .
+      <span className='ml-1'>customized by</span>
+      <a
+        href='https://asami.chiba.jp'
+        className='underline ml-1'
+        target='_blank'
+        rel='noreferrer'
+      >
+        @_asami
+      </a>
+      <span>.</span>
     </div>
   )
 }
